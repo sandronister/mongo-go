e39aac4f2e3cf45fafb7fdc3b96e3a9bc76551c0
Julio Sandroni <sandronister@gmail.com>
2023-12-24 20:17:26 -0300

    improves

--- internal/infra/di/order_di.go
+++ internal/infra/di/order_di.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/sandronister/mongo-go/internal/infra/database"
 	"github.com/sandronister/mongo-go/internal/infra/web/handler"
+	"github.com/sandronister/mongo-go/internal/pkg/enum"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func ConfigOrderHandlerDI(ctx context.Context, mongodb *mongo.Database) *handler.OrderHandler {
-	collection := mongodb.Collection("orders")
+	collection := mongodb.Collection(string(enum.Order))
 	repository := database.NewOrderRepository(collection)
 	return handler.NewOrderHandler(ctx, repository)
 }
