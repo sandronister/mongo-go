5dd3936b9cc29a1e58416dbb0edae3cd13d96e75
Julio Sandroni <sandronister@gmail.com>
2024-01-03 17:27:30 -0300

    fix and improves

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,17 +4,17 @@ import (
 	"context"
 
 	"github.com/sandronister/mongo-go/configs"
+	"github.com/sandronister/mongo-go/internal/infra/database/connection"
+	"github.com/sandronister/mongo-go/internal/infra/web/handler"
 	"github.com/sandronister/mongo-go/internal/infra/web/routes"
-	"github.com/sandronister/mongo-go/pkg/catch"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/sandronister/mongo-go/pkg/errors"
 )
 
 func main() {
 	ctx := context.Background()
-	config, err := configs.LoadConfig(".")
-	catch.Execute(err)
-	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(config.DBHost))
-	catch.Execute(err)
-	routes.HandlerRequest(ctx, config.WebPort, mongoClient.Database(config.DBName))
+	config, err := configs.LoadConfig("../../")
+	errors.Catch(err)
+	database, err := connection.GetDatabase(&ctx, config)
+	errors.Catch(err)
+	routes.HandlerRequest(handler.NewHandlerConfig(&ctx, config, database))
 }
