107063d6ff1016253909eacf416bc7cd4c3d18a2
Julio Sandroni <juliosandroni@MacBook-Pro-de-Julio.local>
2024-04-19 16:51:29 -0300

    fix:using schema to save

--- internal/infra/database/mongo/repository/order_repository.go
+++ internal/infra/database/mongo/repository/order_repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/sandronister/mongo-go/internal/entity"
+	"github.com/sandronister/mongo-go/internal/infra/database/mongo/schemas"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -19,7 +20,14 @@ func NewOrderRepository(collection *mongo.Collection) *OrderRepository {
 
 func (r *OrderRepository) Save(order *entity.Order) error {
 	ctx := context.Background()
-	req, err := r.Collection.InsertOne(ctx, order)
+
+	orderSchmea := schemas.Order{
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.FinalPrice,
+	}
+
+	req, err := r.Collection.InsertOne(ctx, orderSchmea)
 
 	if err != nil {
 		return err
