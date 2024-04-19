166739073b1068c71dc3b053be48e750c4f01b39
Julio Sandroni <juliosandroni@MacBook-Pro-de-Julio.local>
2024-04-19 16:29:35 -0300

    fix:separate entity to schemas

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"github.com/sandronister/mongo-go/configs"
-	"github.com/sandronister/mongo-go/internal/infra/database/connection"
+	"github.com/sandronister/mongo-go/internal/infra/database/mongo/connection"
 	"github.com/sandronister/mongo-go/internal/infra/di"
 	"github.com/sandronister/mongo-go/internal/infra/web/server"
 	"github.com/sandronister/mongo-go/pkg/errors"
 )
 
 func main() {
-	config, err := configs.LoadConfig("../../")
+	config, err := configs.LoadConfig(".")
 	errors.Catch(err)
 	database, err := connection.GetDatabase(config)
 	errors.Catch(err)
